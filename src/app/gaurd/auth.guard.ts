dab3aea5bf340d1033b24298243cdcdcee02c7e9
nagendra <nagendragandham170@gmail.com>
2023-11-02 17:49:06 +0530

    Added login credentials

--- src/app/gaurd/auth.guard.ts
+++ src/app/gaurd/auth.guard.ts
@@ -1,9 +1,9 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  const login=localStorage.getItem('loginCredintials')
-  const router=inject(Router)
+export const authGuard: CanActivateFn = () => {
+  const login=localStorage.getItem('loginCredentials')
+  const router=inject(Router);
   if(login)
   {
     return true;
